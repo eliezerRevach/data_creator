ee4f0720c803c9a949447311237b4311087c2983
elie <ezezer2@hotmail.com>
2021-10-20 19:51:43 +0300

    base

---
+++ websocket.py
@@ -0,0 +1,13 @@
+import asyncio
+from websockets import connect
+import time
+
+async def hello(uri):
+    async with connect(uri) as websocket:
+        while True:
+            msg=await websocket.recv()
+            print(msg)
+
+
+
+asyncio.run(hello("ws://localhost:6437/v7.json"))
